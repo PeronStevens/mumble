69ecbae6f954c2d9df050d9f4209500c739d52d1
= <=>
2017-08-21 17:58:18 -0400

    update

--- Chat/php/cookie.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    echo isset($_COOKIE['username']) ;
-
-?>
--- Chat/php/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $c_value = $_POST['username'];
-
-    $c_name = 'username';
-    
-    setcookie($c_name, $c_value, time() + (86400 * 30));
-
-?>  
