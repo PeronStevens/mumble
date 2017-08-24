d027b679dba950590a07fc030a9eab1e73df1866
Peron Stevens <Peronstevens@yahoo.com>
2017-08-24 12:25:23 -0400

    heroku db testing

--- php/database.php
+++ php/database.php
@@ -4,7 +4,7 @@ $dbopts = parse_url(getenv('DATABASE_URL'));
 
 $host    = "ec2-23-21-197-231.compute-1.amazonaws.com";
 $port    = "5432";
-$db      = "/d10vdtvjln4nv0";
+$db      = "d10vdtvjln4nv0";
 $username = 'ueibvbhyvqjukc';
 $password = '0d180152f6839e0133a451ec791424a6a1e87a5ef6b761248e9844678f046e6e';
 $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
