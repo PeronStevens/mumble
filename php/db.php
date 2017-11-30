916ccbeb6bf2c54d010cd7c76dbdf7cd4c040e43
Peron Stevens <PeronStevens@outlook.com>
2017-11-29 22:55:05 -0500

    update

--- php/db.php
+++ php/db.php
@@ -2,16 +2,16 @@
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
 
-// $host = '127.0.0.1';
-// $db   = 'test';
-// $username = 'Admin';
-// $password = '';
+$host = '127.0.0.1';
+$db   = 'test';
+$username = 'Admin';
+$password = '';
 
-$host    = "ec2-23-21-197-231.compute-1.amazonaws.com";
-$port    = "5432";
-$db      = "d10vdtvjln4nv0";
-$username = 'ueibvbhyvqjukc';
-$password = '0d180152f6839e0133a451ec791424a6a1e87a5ef6b761248e9844678f046e6e';
+// $host    = "ec2-23-21-197-231.compute-1.amazonaws.com";
+// $port    = "5432";
+// $db      = "d10vdtvjln4nv0";
+// $username = 'ueibvbhyvqjukc';
+// $password = '0d180152f6839e0133a451ec791424a6a1e87a5ef6b761248e9844678f046e6e';
 $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
 
 try{
